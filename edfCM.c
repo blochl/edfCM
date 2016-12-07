354ab063e1f6278ab9302496cc1fe8c2d4c68ac8
Leonid Bloch <leonid.bloch@esrf.fr>
2016-12-07 12:19:43 +0100

    Add logic to determine the number of bytes read
    Previously, the number of bytes read each time was 128 (constant). This
    number was determined by trial and error, for performance
    considerations. This required DIM_1 (the x dimension of the images) to
    be divisible by (128/(image depth)). Such an approach was based on the
    assumption that the dimensions of an image are 2**n, n>6 (which is true
    for most EDF images). An error message was printed for other images.
    
    Currently, the minimal number of bytes read is 128 (or less, if the
    dimensions of an image are too small for that) and if this does not
    satisfy the requirement, this number will go up, until it will satisfy
    it.
    
    Signed-off-by: Leonid Bloch <leonid.bloch@esrf.fr>

--- edfCM.c
+++ edfCM.c
@@ -8,10 +8,12 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_HEAD_SIZE  8192
-#define READBYTES      128  /* Dim_1 and head length must be divisible by it */
-#define S_SIZE         sizeof("Size =")
-#define S_DIM          sizeof("Dim_X =")
+#define READBYTES_DAT_MIN  128 /* Def. value is 128, must be divisible by 4  */
+#define READBYTES_HDR      128 /* Hdr must be divisible by it (must for EDF) */
+#define MAX_HEAD_SIZE      8192
+#define S_SIZE             sizeof("Size =")
+#define S_DIM              sizeof("Dim_X =")
+#define MIN(a,b)           (((a) < (b)) ? (a) : (b))
 
 typedef struct rec
 {
@@ -30,84 +32,84 @@ typedef struct meta
 
 typedef union aval
 {
-    char av_1[READBYTES];
-    uint16_t av_2[READBYTES / 2];
-    uint32_t av_4[READBYTES / 4];
+    char *av_1;
+    uint16_t *av_2;
+    uint32_t *av_4;
 } aval;
 
 static inline void
-parsehead(FILE *p_str, meta *meta_curr)
+parsehead(FILE *p_str, aval **curr_aval, meta *meta_curr, int *readbytes)
 {
     char hdr[MAX_HEAD_SIZE] = {0};
     int headlen = 0;
     do {
-        fread(&hdr[headlen * READBYTES], 1, READBYTES, p_str);
+        fread(&hdr[headlen * READBYTES_HDR], 1, READBYTES_HDR, p_str);
         headlen++;
-    } while( hdr[headlen * READBYTES - 2] != '}' );
+    } while( hdr[headlen * READBYTES_HDR - 2] != '}' );
     int size = atoi(strstr(hdr, "Size") + S_SIZE);
     int dim1 = atoi(strstr(hdr, "Dim_1") + S_DIM);
     int dim2 = atoi(strstr(hdr, "Dim_2") + S_DIM);
     meta_curr->Dim_1 = dim1;
     meta_curr->Dim_2 = dim2;
     meta_curr->depth = size / dim1 / dim2;
-    meta_curr->headlen = headlen * READBYTES;
+    meta_curr->headlen = headlen * READBYTES_HDR;
     if ( meta_curr->depth != 1 && meta_curr->depth != 2
          && meta_curr->depth != 4 ) {
         fprintf(stderr, "Depth must be only 1, 2, or 4, but got: %d\n",
                 meta_curr->depth);
         exit(1);
     }
-    if ( dim1 % (READBYTES / meta_curr->depth) ) {
-        printf("DIM_1 must be dividable by %d, but got: %d\n",
-               (READBYTES / meta_curr->depth), dim1);
-        exit(1);
+    readbytes[0] = MIN(READBYTES_DAT_MIN, dim1 * meta_curr->depth);
+    while ( (dim1 * meta_curr->depth) % readbytes[0] ) {
+        readbytes[0] += meta_curr->depth;
     }
+    *curr_aval = malloc(readbytes[0]);
+    (*curr_aval)->av_1 = malloc(readbytes[0]);
 }
 
 static inline void
 skiphead(FILE *p_str, const meta *meta_curr)
 {
-    static char buf[READBYTES];
-    fread(&buf[0], 1, READBYTES, p_str);
+    static char buf[READBYTES_HDR];
+    fread(&buf[0], 1, READBYTES_HDR, p_str);
     if ( buf[0] != '{' ) {
         exit(0);
     }
     int i;
-    for ( i = 1; i < meta_curr->headlen / READBYTES; i++ )
+    for ( i = 1; i < meta_curr->headlen / READBYTES_HDR; i++ )
     {
-        fread(&buf[0], 1, READBYTES, p_str);
+        fread(&buf[0], 1, READBYTES_HDR, p_str);
     }
 }
 
 static inline void
-calc(FILE *p_str, const meta *meta_curr)
+calc(FILE *p_str, aval *curr_aval, const meta *meta_curr, const int *readbytes)
 {
     int i, j, k;
     double x_c, y_c;
-    const int step = READBYTES / meta_curr->depth;
+    const int step = readbytes[0] / meta_curr->depth;
     const int dim_1_rb = meta_curr->Dim_1 / step;
     uint64_t tval = 0;
     rec curr_rec = {0};
-    aval curr_aval;
     for ( i = 1; i <= meta_curr->Dim_2; i++ )
     {
         for ( j = 0; j < dim_1_rb; j++ )
         {
-            fread(&curr_aval.av_1[0], 1, READBYTES, p_str);
+            fread(&curr_aval->av_1[0], 1, readbytes[0], p_str);
             for ( k = 0; k < step; k++ )
             {
                 switch ( meta_curr->depth ) {
                     case 1:
-                        curr_rec.x += (j * step + k + 1) * curr_aval.av_1[k];
-                        tval += curr_aval.av_1[k];
+                        curr_rec.x += (j * step + k + 1) * curr_aval->av_1[k];
+                        tval += curr_aval->av_1[k];
                     break;
                     case 2:
-                        curr_rec.x += (j * step + k + 1) * curr_aval.av_2[k];
-                        tval += curr_aval.av_2[k];
+                        curr_rec.x += (j * step + k + 1) * curr_aval->av_2[k];
+                        tval += curr_aval->av_2[k];
                     break;
                     case 4:
-                        curr_rec.x += (j * step + k + 1) * curr_aval.av_4[k];
-                        tval += curr_aval.av_4[k];
+                        curr_rec.x += (j * step + k + 1) * curr_aval->av_4[k];
+                        tval += curr_aval->av_4[k];
                     break;
                 }
             }
@@ -140,12 +142,14 @@ int main( int argc, char *argv[] )
                "The images can be in one .edf file, or several.\n\n", argv[0]);
         return 1;
     }
+    static int readbytes[1];
     meta *meta_curr = malloc(sizeof(meta));
+    aval *curr_aval;
     int g = atoi(argv[1]);
-    parsehead(stdin, meta_curr);
+    parsehead(stdin, &curr_aval, meta_curr, readbytes);
     while(g)
     {
-        calc(stdin, meta_curr);
+        calc(stdin, curr_aval, meta_curr, readbytes);
         switch ( g ) {
             case 1:
             break;
@@ -155,5 +159,7 @@ int main( int argc, char *argv[] )
         g--;
     }
     free(meta_curr);
+    free(curr_aval->av_1);
+    free(curr_aval);
     return 0;
 }
